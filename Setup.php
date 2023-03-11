68d81c7f0da96eae15c8b56963494079c35ae57e
Oleguer Mas <oleguer.ml89@gmail.com>
2023-03-11 20:10:18 +0100

    php cmd.php xf-addon:create

---
+++ Setup.php
@@ -0,0 +1,23 @@
+<?php declare(strict_types=1);
+
+namespace olml89\Subscriptions;
+
+use XF\AddOn\AbstractSetup;
+
+class Setup extends AbstractSetup
+{
+    public function install(array $stepParams = [])
+	{
+		// TODO: Implement install() method.
+	}
+
+	public function upgrade(array $stepParams = [])
+	{
+		// TODO: Implement upgrade() method.
+	}
+
+	public function uninstall(array $stepParams = [])
+	{
+		// TODO: Implement uninstall() method.
+	}
+}
