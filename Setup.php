bad55f21bec9ed925bcda02f6dd074b7082921a8
Oleguer Mas <oleguer.ml89@gmail.com>
2023-03-12 00:32:29 +0100

    Create table xf_subscriptions

--- Setup.php
+++ Setup.php
@@ -3,21 +3,28 @@
 namespace olml89\Subscriptions;
 
 use XF\AddOn\AbstractSetup;
+use XF\Db\Schema\Create;
 
 class Setup extends AbstractSetup
 {
-    public function install(array $stepParams = [])
+    public function install(array $stepParams = []): void
 	{
-		// TODO: Implement install() method.
+        $this->schemaManager()->createTable('xf_subscriptions', function(Create $table) : void {
+            $table->addColumn('id', 'int')->primaryKey()->autoIncrement();
+            $table->addColumn('user_id', 'int')->nullable(FALSE);
+            $table->addColumn('token', 'varchar', 32)->nullable(FALSE);
+            $table->addColumn('webhook', 'varchar', 255)->nullable(FALSE);
+            $table->addUniqueKey('user_id');
+        });
 	}
 
-	public function upgrade(array $stepParams = [])
+	public function upgrade(array $stepParams = []): void
 	{
 		// TODO: Implement upgrade() method.
 	}
 
-	public function uninstall(array $stepParams = [])
+	public function uninstall(array $stepParams = []): void
 	{
-		// TODO: Implement uninstall() method.
+        $this->schemaManager()->dropTable('xf_subscriptions');
 	}
 }
